28a3e4a3882586a8262b826732711415b5cab453
deadem <deadem@gmail.com>
2018-10-08 12:51:41 +0300

    добавлен контроль за всеми стиками

--- input.py
+++ input.py
@@ -8,7 +8,8 @@ class Direction:
 
 class Input:
     state = {Direction.x: 0}
-    axis = [0]
+    axis0 = 0
+    axis = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     def __init__(self):
         joystick = pygame.joystick.Joystick(0)
@@ -18,8 +19,8 @@ class Input:
         self.state[direction] = value
 
     def move_axis0(self, value):
-        axis = self.axis[0]
-        self.axis[0] = value
+        axis = self.axis0
+        self.axis0 = value
         if abs(value) > 0.3:
             if value > 0.5 and value > axis:
                 self.set_state(Direction.x, 1)
@@ -28,6 +29,11 @@ class Input:
         else:
             self.set_state(Direction.x, 0)
 
+    def move_axis(self, num, value):
+        if abs(value) < 0.3:
+            value = 0
+        self.axis[num] = value
+
     def move_keys(self, event):
         if event.key == 275:
             self.set_state(Direction.x, 1 if event.type == pygame.KEYDOWN else 0)
@@ -35,11 +41,17 @@ class Input:
             self.set_state(Direction.x, -1 if event.type == pygame.KEYDOWN else 0)
 
     def event(self, event):
-        if event.type == pygame.JOYAXISMOTION and event.axis == 0:
-            self.move_axis0(event.value)
+        if event.type == pygame.JOYAXISMOTION:
+            self.move_axis(event.axis, event.value)
+            if event.axis == 0:
+                self.move_axis0(event.value)
+
         if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
             self.move_keys(event)
 
     def test(self, x=False):
         if x:
             return self.state[Direction.x]
+
+    def get_axis(self, num):
+        return self.axis[num]
